8a1b32b8aee6973cdd39dc830ce73f27a1ef063f
Neha Rana <121093178+neha13rana@users.noreply.github.com>
2025-11-28 12:13:12 +0530

    Add experience details to About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -45,6 +45,7 @@ const About = () => {
                 </div>
                 <div>
                   <h3 className="font-bold text-lg mb-1">Experience</h3>
+                  <p className="text-gray-600">- CTO at Aethenix</p>
                   <p className="text-gray-600">- AI Developer Intern at REConnect Energy Solutions</p>
                   <p className="text-gray-600">- Web Developer Intern at iShape Technolab</p>
                   <p className="text-gray-600">- Team Lead, Developer for the project of Student Counselling Chatbot (For the education department-Government of Gujarat)</p>
