7a55673eda701dfcd87c7f59b85761b02aa47aa3
Neha Rana <121093178+neha13rana@users.noreply.github.com>
2026-04-16 17:55:45 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
-import Skills from '@/components/Skills';
+// import Skills from '@/components/Skills';
 import Experience from '@/components/Experience';
 import Projects from '@/components/Projects';
 import Achievements from '@/components/Achievements';
